e99a3644013332ac422252d08783c78b099a31c6
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-03-29 02:25:58 +0900

    show post time of a message

--- config/routes.io.js
+++ config/routes.io.js
@@ -9,6 +9,21 @@ module.exports = function(app) {
     return req.handshake.headers['x-forwarded-for'] || req.handshake.address.address;
   }
 
+  function zeropadding(num, count) {
+    return (Array(count).join('0') + num).slice(-count);
+  }
+
+  function datestring(datetime) {
+    var padding = 2;
+    var year = datetime.getFullYear();
+    var month = zeropadding(datetime.getMonth() + 1, padding); // Months are zero based
+    var day = zeropadding(datetime.getDate(), padding);
+    var hours = zeropadding(datetime.getHours(), padding);
+    var minutes = zeropadding(datetime.getMinutes(), padding);
+    var seconds = zeropadding(datetime.getSeconds(), padding);
+    return year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ':' + seconds;
+  }
+
   app.io.sockets.on('connection', function(req) {
     user_name = ipaddress(req);
 
@@ -19,7 +34,7 @@ module.exports = function(app) {
       var talks = [];
       messages.forEach(function(message) {
         user = message.user;
-        talks.push({ user_name: user.name, message: message.body });
+        talks.push({ user_name: user.name, message: message.body, created_at: datestring(message.created_at) });
       });
       app.io.broadcast('talk.log', talks);
     });
@@ -35,7 +50,7 @@ module.exports = function(app) {
       message = new Message({ user: user, body: message_body });
       message.save();
 
-      app.io.broadcast('talk', { user_name: user_name, message: message_body });
+      app.io.broadcast('talk', { user_name: user_name, message: message_body, created_at: datestring(new Date) });
 
       console.log('talk: @' + user_name + ' say ' + message_body);
     });
